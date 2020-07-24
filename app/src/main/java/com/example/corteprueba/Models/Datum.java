d2a7e3acd0e036643069e1405a4896ffa00f6a90
RubenJayaP <66756477+RJayaD@users.noreply.github.com>
2020-07-23 19:29:29 -0500

    creacion clase Datum

---
+++ app/src/main/java/com/example/corteprueba/Models/Datum.java
@@ -0,0 +1,26 @@
+package com.example.corteprueba.Models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Datum {
+    private String name;
+    private String alpha2Code;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAlpha2Code() {
+        return alpha2Code;
+    }
+    public void setAlpha2Code(String alpha2Code) {
+        this.alpha2Code = alpha2Code;
+    }
+
+
+
+}
